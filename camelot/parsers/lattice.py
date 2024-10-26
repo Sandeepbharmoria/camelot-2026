py-pdf/pypdf_table_extraction

Remove redundant conditions on `Cell.vspan/hspan`
The condition `hspan and not left` expands to `(not left or not right)
and not left` which simplifies to `not left`.

After this change `Cell.hspan` and `Cell.vspan` aren't used anymore in
this codebase. They stay because they are part of the API.

camelot/parsers/lattice.py
@@ -181,16 +181,16 @@ class Lattice(BaseParser):
         indices = []
         for r_idx, c_idx, text in idx:
             for d in shift_text:
-                if d == "l" and table.cells[r_idx][c_idx].hspan:
+                if d == "l":
                     while not table.cells[r_idx][c_idx].left:
                         c_idx -= 1
-                if d == "r" and table.cells[r_idx][c_idx].hspan:
+                elif d == "r":
                     while not table.cells[r_idx][c_idx].right:
                         c_idx += 1
-                if d == "t" and table.cells[r_idx][c_idx].vspan:
+                elif d == "t":
                     while not table.cells[r_idx][c_idx].top:
                         r_idx -= 1
-                if d == "b" and table.cells[r_idx][c_idx].vspan:
+                elif d == "b":
                     while not table.cells[r_idx][c_idx].bottom:
                         r_idx += 1
             indices.append((r_idx, c_idx, text))
@@ -219,13 +219,13 @@ class Lattice(BaseParser):
                 for i in range(len(table.cells)):
                     for j in range(len(table.cells[i])):
                         if table.cells[i][j].text.strip() == "":
-                            if table.cells[i][j].hspan and not table.cells[i][j].left:
+                            if not table.cells[i][j].left:
                                 table.cells[i][j].text = table.cells[i][j - 1].text
             elif f == "v":
                 for i in range(len(table.cells)):
                     for j in range(len(table.cells[i])):
                         if table.cells[i][j].text.strip() == "":
-                            if table.cells[i][j].vspan and not table.cells[i][j].top:
+                            if not table.cells[i][j].top:
                                 table.cells[i][j].text = table.cells[i - 1][j].text
         return table
 

Sandeepbharmoria/camelot-2026

camelot/parsers/lattice.py
@@ -145,19 +145,19 @@ class Lattice(BaseParser):
         tuple
             New row and column indices after the shift.
         """
-        if direction == "l" and table.cells[r_idx][c_idx].hspan:
+        if direction == "l":
             while c_idx > 0 and not table.cells[r_idx][c_idx].left:
                 c_idx -= 1
-        elif direction == "r" and table.cells[r_idx][c_idx].hspan:
+        elif direction == "r":
             while (
                 c_idx < len(table.cells[r_idx]) - 1
                 and not table.cells[r_idx][c_idx].right
             ):
                 c_idx += 1
-        elif direction == "t" and table.cells[r_idx][c_idx].vspan:
+        elif direction == "t":
             while r_idx > 0 and not table.cells[r_idx][c_idx].top:
                 r_idx -= 1
-        elif direction == "b" and table.cells[r_idx][c_idx].vspan:
+        elif direction == "b":
             while r_idx < len(table.cells) - 1 and not table.cells[r_idx][c_idx].bottom:
                 r_idx += 1
 